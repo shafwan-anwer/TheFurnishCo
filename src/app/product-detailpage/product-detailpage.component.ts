1d5f10b339d753a54118b35044c532e6633f9d62
ShafwanAnwer <shafwan.anwer@gmail.com>
2023-05-28 11:52:55 +0530

    main

--- src/app/product-detailpage/product-detailpage.component.ts
+++ src/app/product-detailpage/product-detailpage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-detailpage',
@@ -6,4 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detailpage.component.scss']
 })
 export class ProductDetailpageComponent {
+
+  imageUrl: SafeUrl;
+  smallImage1: SafeUrl;
+  smallImage2: SafeUrl;
+  smallImage3: SafeUrl;
+  constructor(private sanitizer: DomSanitizer) {
+
+    this.imageUrl = this.sanitizer.bypassSecurityTrustUrl('assets/product-pagebig.png');
+    this.smallImage2 = this.sanitizer.bypassSecurityTrustUrl('assets/small-productimg2.png');
+    this.smallImage1 = this.sanitizer.bypassSecurityTrustUrl('assets/small-productimg1.png');
+    this.smallImage3 = this.sanitizer.bypassSecurityTrustUrl('assets/small-productimg3.png');
+
+  }
 }
